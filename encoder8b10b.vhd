1881fa766e938d40609ef6296fbf392863e141f1
kedziorno <kjoblak85@gmail.com>
2022-09-12 18:22:33 +0200

    enc8b10b

--- encoder8b10b.vhd
+++ encoder8b10b.vhd
@@ -31,22 +31,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity encoder8b10b is
 port (
-pcmpls4,pndfs6,ncmpls6 : in std_logic;
 pk : in std_logic;
+pndfs6 : in std_logic;
 ipt : in std_logic_vector(7 downto 0);
-opt : out std_logic_vector(9 downto 0)
+opt : out std_logic_vector(9 downto 0);
+pbalby : out std_logic
 );
 end entity encoder8b10b;
 
 -- XXX US6977599.pdf
 -- XXX 8B/10B ENCODING AND DECODING FOR HIGH SPEED APPLICATIONS
 architecture Behavioral of encoder8b10b is
+	signal pcmpls4,ncmpls6 : std_logic;
 	signal pa,pb,pc,pd,pe,pf,pg,ph : std_logic;
 	signal pca,pcb,pcc,pcd,pce,pcf,pcg,pch,pci,pcj : std_logic;
 	signal na,nb,nc,nd,ne,nf : std_logic;
 	signal n10,n9,n8,n6,n5,n4,n3,n2,n1,n0 : std_logic;
-	signal a0,a2,a4 : std_logic;
-	signal pl03,pl30,pl21,pfnhn,pbf,nbg,nj,nl30d,peodn,nl12dnen,nl03d,pdnen,pdne,nde,nden,nbb,pfgh,nl03den,pde,nbcpeodn,pbi,pkoduee,pdenfgh,nbc,nbd,pbe,pbipfgh,nfgh,nk,nfghk,pfghasak,pdfs6,ndne,pfghnk,pdnefgh,pl12 : std_logic;
+	signal pl03,pl30,pl21,pfnhn,pbf,nbg,nj,nl30d,peodn,nl12dnen,nl03d,pdnen,pdne,nde,nden,nbb,pfgh,nl03den,pde,pbi,pkoduee,pdenfgh,nbc,nbd,pbe,nfgh,nk,nfghk,pfghasak,pdfs6,ndne,pdnefgh,pl12 : std_logic;
+	signal pfueg,pndfs6adnen,pndfs6aden,pndfs6adoen,pdfs6ade,pdfs6aeodn,pdfss6aedn,pdoeakn,ng,npdrs4,pdrs4apdfs6,pdrs4andfs6,pndrs4andfs6,pndrs4apdfs6,pndfapdrs6,pdfandrs6,pdfsd6aedn,pbals6,nbals6,nbals4,nfghn,ndrs4 : std_logic;
 begin
 	aa1 : pa <= ipt(0);
 	aa2 : pb <= ipt(1);
@@ -56,16 +58,16 @@ begin
 	aa6 : pf <= ipt(5);
 	aa7 : pg <= ipt(6);
 	aa8 : ph <= ipt(7);
-	bb1 : opt(0) <= pca;
-	bb2 : opt(1) <= pcb;
-	bb3 : opt(2) <= pcc;
-	bb4 : opt(3) <= pcd;
-	bb5 : opt(4) <= pce;
-	bb6 : opt(5) <= pci;
-	bb7 : opt(6) <= pcf;
-	bb8 : opt(7) <= pcg;
-	bb9 : opt(8) <= pch;
-	bb10 : opt(9) <= pcj;
+	bb1 : opt(9) <= pca;
+	bb2 : opt(8) <= pcb;
+	bb3 : opt(7) <= pcc;
+	bb4 : opt(6) <= pcd;
+	bb5 : opt(5) <= pce;
+	bb6 : opt(4) <= pci;
+	bb7 : opt(3) <= pcf;
+	bb8 : opt(2) <= pcg;
+	bb9 : opt(1) <= pch;
+	bb10 : opt(0) <= pcj;
 	a301 : nb <= not pb;
 	a302 : na <= not pa;
 	a303 : nc <= not pc;
@@ -78,14 +80,14 @@ begin
 	a316 : n1 <= not (na and nc and pb);
 	a315 : n0 <= not (nb and nb and pa);
 	a323 : pl21 <= (not n3) or (not n4) or (not n5);
-	a322 : a0 <= (not n0) or (not n1) or (not n2);
+	a322 : pl12 <= (not n0) or (not n1) or (not n2);
 	a304 : nd <= not pd;
 	a306 : ne <= not pe;
 	a305 : nf <= not pf;
 	a321 : nfgh <= not (pf and pg and ph);
 	a346 : pfnhn <= (not pf) and (not ph);
-	a375 : n10 <= a2 and (not ph);
-	a370 : a2 <= pf xor pg;
+	a375 : n10 <= pfueg and (not ph);
+	a370 : pfueg <= pf xor pg;
 	a349 : pbf <= (not pfghasak) and (not nf);
 	a348 : nbg <= not (pfnhn or pg);
 	a347 : nj <= not (n10 or pfghasak);
@@ -109,7 +111,7 @@ begin
 	a311 : ndne <= not pdne;
 	a381 : nbc <= not ((pl03 and peodn) or pc);
 	a333 : pbe <= (not nl12dnen) or (not n6) or (not nl03den);
-	a385 : pbi <= ((pl21 and pdnen) or (a0 and pkoduee) or (pl03 and pdne) or (pl30 and pde));
+	a385 : pbi <= ((pl21 and pdnen) or (pl21 and pkoduee) or (pl03 and pdne) or (pl30 and pde));
 	a353 : pdenfgh <= (not nfgh) and (not nden);
 	a357 : pca <= not (ncmpls6 xor pa);
 	a367 : pcb <= ncmpls6 xor nbb;
@@ -126,5 +128,28 @@ begin
 	a336 : n9 <= not (pdfs6 and pl21 and pdenfgh);
 	a335 : n8 <= not (pndfs6 and pl12 and pdnefgh);
 	a310 : pdfs6 <= not pndfs6;
-	
+	b320 : pndfs6adnen <= (not pndfs6) and (not pd) and (not pe);
+	b321 : pndfs6aden <= (not nden) and (not pndfs6);
+	b322 : pndfs6adoen <= (not pdne) and (not pndfs6);
+	b323 : pdfs6ade <= (not nde) and (not pndfs6);
+	b360 : pdfs6aeodn <= not ((not pd or not ne) and pndfs6);
+	b324 : pdfss6aedn <= (not pndfs6) and (not ne) and (not pd);
+	aaaa : pdfs6 <= not pndfs6;
+	b370 : pdoeakn <= (pd or ne) and nk;
+	b301 : ng <= not pg;
+	b361 : npdrs4 <= not ((nf and ng) or (pfueg and pk));
+	b325 : pdrs4apdfs6 <= (not npdrs4) and (not pndfs6); -- XXX
+	b326 : pdrs4andfs6 <= (not npdrs4) and (not pdfs6); -- XXX
+	b327 : pndrs4andfs6 <= (not ndrs4) and (not pdfs6); -- XXX
+	b328 : pndrs4apdfs6 <= (not pndfs6) and (not ndrs4);
+	b350 : pndfapdrs6 <= (pl12 and pndfs6adnen) or (pl30 and pndfs6aden) or (pndfs6adoen and pl03);
+	b351 : pdfandrs6 <= (pl21 and pdfs6ade) or (pl30 and pdfs6aeodn) or (pl03 and pdfss6aedn) or (pdfs6 and pk);
+	b330 : ncmpls6 <= not (pndfapdrs6 or pdfandrs6);
+	b352 : pbals6 <= (pdoeakn and pl12) or (pl21 and nde) or (pl30 and pdnen);
+	b302 : nbals6 <= not pbals6;
+	b340 : pbalby <= pbals6 xor nbals4;
+	b329 : nbals4 <= not (pfueg or nfghn);
+	b353 : pcmpls4 <= (nbals6 and pdrs4apdfs6) or (nbals6 and pndrs4andfs6) or (pbals6 and pdrs4andfs6) or (pbals6 and pndrs4apdfs6);
+	b331 : nfghn <= (not ph) and (not ndrs4);
+	b310 : ndrs4 <= not (pf and pg);
 end architecture Behavioral;
--- tb_encoder8b10b.vhd
+++ tb_encoder8b10b.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   14:52:24 09/12/2022
+-- Create Date:   17:16:50 09/12/2022
 -- Design Name:   
 -- Module Name:   /home/user/workspace/pata_sata/tb_encoder8b10b.vhd
 -- Project Name:  pata_sata
@@ -40,24 +40,22 @@ ARCHITECTURE behavior OF tb_encoder8b10b IS
 -- Component Declaration for the Unit Under Test (UUT)
 COMPONENT encoder8b10b
 PORT(
-pcmpls4 : IN  std_logic;
-pndfs6 : IN  std_logic;
-ncmpls6 : IN  std_logic;
 pk : IN  std_logic;
+pndfs6 : IN  std_logic;
 ipt : IN  std_logic_vector(7 downto 0);
-opt : OUT  std_logic_vector(9 downto 0)
+opt : OUT  std_logic_vector(9 downto 0);
+pbalby : OUT  std_logic
 );
 END COMPONENT;
 
 --Inputs
-signal pcmpls4 : std_logic := '0';
-signal pndfs6 : std_logic := '0';
-signal ncmpls6 : std_logic := '0';
 signal pk : std_logic := '0';
+signal pndfs6 : std_logic := '0';
 signal ipt : std_logic_vector(7 downto 0) := (others => '0');
 
 --Outputs
 signal opt : std_logic_vector(9 downto 0);
+signal pbalby : std_logic;
 
 constant clock_period : time := 10 ns;
 signal clock : std_logic;
@@ -66,13 +64,15 @@ BEGIN
 
 -- Instantiate the Unit Under Test (UUT)
 uut: encoder8b10b PORT MAP (
-pcmpls4 => pcmpls4,
-pndfs6 => pndfs6,
-ncmpls6 => ncmpls6,
 pk => pk,
+pndfs6 => pndfs6,
 ipt => ipt,
-opt => opt
+opt => opt,
+pbalby => pbalby
 );
+
+pk <= '0';
+pndfs6 <= '0';
 
 -- Clock process definitions
 clock_process :process
@@ -83,19 +83,22 @@ clock <= '1';
 wait for clock_period/2;
 end process;
 
-pk <= '0';
-pndfs6 <= '0';
-pcmpls4 <= '0';
-ncmpls6 <= '0';
-
 -- Stimulus process
-stim_proc : process
+stim_proc: process
 begin
 -- hold reset state for 100 ns.
 wait for 100 ns;
 wait for clock_period*10;
 -- insert stimulus here
+ipt <= x"45"; wait for clock_period;
 ipt <= "11010101"; wait for clock_period;
+ipt <= "11101010"; wait for clock_period;
+ipt <= "00101010"; wait for clock_period;
+ipt <= "00010101"; wait for clock_period;
+ipt <= "10101011"; wait for clock_period;
+ipt <= "01010111"; wait for clock_period;
+ipt <= "10101000"; wait for clock_period;
+ipt <= "01010100"; wait for clock_period;
 wait;
 end process;
 
