58d6b84265aedb407c112ef8e481e041b567b4db
kedziorno <kjoblak85@gmail.com>
2023-07-21 23:55:00 +0200

    files

---
+++ module681.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:16:14 07/21/2023 
+-- Design Name: 
+-- Module Name:    module681 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity module681 is
+end module681;
+
+architecture Behavioral of module681 is
+
+begin
+
+
+end Behavioral;
+
