6ff8d04e2beb7ea64ea07e1f78141fc16539c839
kedziorno <kjoblak85@gmail.com>
2024-11-26 13:48:51 +0100

    14.7

--- tb_MX25V1006F.vhd
+++ tb_MX25V1006F.vhd
@@ -29,70 +29,62 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.ALL;
- 
+
 ENTITY tb_MX25V1006F IS
 END tb_MX25V1006F;
- 
-ARCHITECTURE behavior OF tb_MX25V1006F IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT MX25V1006F
-    PORT(
-         SCLK : IN  std_logic;
-         CS : IN  std_logic;
-         SI : INOUT  std_logic;
-         SO : INOUT  std_logic;
-         WP : INOUT  std_logic
-        );
-    END COMPONENT;
-    
 
-   --Inputs
-   signal SCLK : std_logic := '0';
-   signal CS : std_logic := '0';
+ARCHITECTURE behavior OF tb_MX25V1006F IS
 
-	--BiDirs
-   signal SI : std_logic;
-   signal SO : std_logic;
-   signal WP : std_logic;
- 
+-- Component Declaration for the Unit Under Test (UUT)
+COMPONENT MX25V1006F
+PORT(
+SCLK : IN  std_logic;
+CS : IN  std_logic;
+SI : INOUT  std_logic;
+SO : INOUT  std_logic;
+WP : INOUT  std_logic
+);
+END COMPONENT;
+
+--Inputs
+signal SCLK : std_logic := '0';
+signal CS : std_logic := '0';
+
+--BiDirs
+signal SI : std_logic;
+signal SO : std_logic;
+signal WP : std_logic;
+
+constant clock_period : time := 1 ns;
+signal clock : std_logic := '0';
+
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: MX25V1006F PORT MAP (
-          SCLK => SCLK,
-          CS => CS,
-          SI => SI,
-          SO => SO,
-          WP => WP
-        );
- 
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
- 
-   constant <clock>_period := 1ns;
- 
-   <clock>_process :process
-   begin
-		<clock> <= '0';
-		wait for <clock>_period/2;
-		<clock> <= '1';
-		wait for <clock>_period/2;
-   end process;
- 
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100ms.
-      wait for 100ms;	
+-- Instantiate the Unit Under Test (UUT)
+uut: MX25V1006F PORT MAP (
+SCLK => SCLK,
+CS => CS,
+SI => SI,
+SO => SO,
+WP => WP
+);
 
-      wait for <clock>_period*10;
+clock_process : process
+begin
+clock <= '0';
+wait for clock_period/2;
+clock <= '1';
+wait for clock_period/2;
+end process clock_process;
 
-      -- insert stimulus here 
-
-      wait;
-   end process;
+-- Stimulus process
+stim_proc : process
+begin
+-- hold reset state for 100ms.
+wait for 100ms;
+wait for clock_period*10;
+-- insert stimulus here
+wait;
+end process stim_proc;
 
 END;
